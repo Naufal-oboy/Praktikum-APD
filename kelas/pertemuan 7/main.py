6535b3cf457814a3f6a6dc2f60a997db90b7b91f
Naufal-oboy <xorpro08@gmail.com>
2024-10-18 15:08:15 +0800

    pertemuan 7

---
+++ kelas/pertemuan 7/main.py
@@ -0,0 +1,34 @@
+# def hasil(nama):
+#     print(" hello world " + nama )
+
+
+# hasil ("naufal")
+# hasil ("oby")
+# hasil("oboy")
+
+def luas_persegi_panjang(panjang, lebar):
+    luas = panjang * lebar
+    print (f"luas persegi panjang dengan panjnag {panjang} dan lebar {lebar} ", luas)
+    return luas
+
+
+luas_persegi = luas_persegi_panjang(10,2)
+print(luas_persegi)
+
+# nama = "dimas"
+
+# def say_hello():
+#     nama = "daffa"
+#     print (nama, "dalam fungsi")
+
+# print (nama, "lluar fungsi") 
+
+# say_hello()
+
+# def faktorial(n):
+#     if n == 1:
+#         return 1
+#     else:
+#         return n * faktorial(n-1)
+    
+# print(faktorial())
